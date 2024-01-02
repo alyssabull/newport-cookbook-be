753c57dcfcbe5fbe7c7db88bed5a7afda188abed
alyssabull <alyssabull@gmail.com>
2024-01-02 10:05:58 -0700

    Update connection string

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const cors = require("cors");
 const env = require("dotenv").config();
 const app = express();
 
-const port = process.env.DBPORT || 3001;
+// const port = process.env.DBPORT || 3001;
 // const host = process.env.DBHOST || "0.0.0.0";
-// const railwayURL = `mysql://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}:${process.env.DBPORT}/${process.env.DBDATABASE}`;
-// const db = mysql.createPool(railwayURL);
+const railwayURL = `mysql://ucxpdofnzajyujd8:4c7WqUFbmrYEnZQwSkW0@b32cy2ewfhbtxzwsgdpo-mysql.services.clever-cloud.com:3306/b32cy2ewfhbtxzwsgdpo`;
+const db = mysql.createPool(railwayURL);
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.get('/', async (req, res) => {
-//     res.send("welcome to the server");
-//     res.end();
-// });
+app.get('/', async (req, res) => {
+    res.send("welcome to the server");
+    res.end();
+});
 
-// app.get('/getmovies', async (req, res) => {
-//     const sqlGet = "SELECT * FROM `movie_reviews4`";
-//     const result = await db.query(sqlGet);
-//     res.send(JSON.stringify(result));
-//     res.end();
-// });
+app.get('/getmovies', async (req, res) => {
+    const sqlGet = "SELECT * FROM `movie_reviews4`";
+    const result = await db.query(sqlGet);
+    res.send(JSON.stringify(result));
+    res.end();
+});
 
-app.listen(port, function () {
-    console.log(`running on port ${port}`)
+app.listen(3306, function () {
+    console.log(`running on port 3306`)
 });
