bff0cf9dbabad355bcf69caf57eca8605aab1bd6
alyssabull <alyssabull@gmail.com>
2024-01-02 12:07:33 -0700

    Update cors again

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ var db = mysql.createConnection({
 });
 
 app.use(cors());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "https://poetic-sable-dac553.netlify.app");
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization, Access-Control-Allow-Origin');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+    // Pass to next layer of middleware
+    next();
+  });
 app.use(express.json());
 
 app.get('/', async (req, res) => {
