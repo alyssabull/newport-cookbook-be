9cc519f323322a9fefcd36a77bfcde2d394c5b34
alyssabull <alyssabull@gmail.com>
2024-01-02 15:18:35 -0700

    Update host

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 
 const port = process.env.MYSQL_ADDON_PORT || 8080;
 const host = process.env.MYSQL_ADDON_HOST || "0.0.0.0";
-// const cleverCloudURL = `mysql://${process.env.MYSQL_ADDON_USER}:${process.env.MYSQL_ADDON_PASSWORD}@${process.env.MYSQL_ADDON_HOST}:${process.env.MYSQL_ADDON_PORT}/${process.env.MYSQL_ADDON_DB}`;
-// const db = mysql.createPool(cleverCloudURL);
+const cleverCloudURL = `mysql://${process.env.MYSQL_ADDON_USER}:${process.env.MYSQL_ADDON_PASSWORD}@${process.env.MYSQL_ADDON_HOST}:${process.env.MYSQL_ADDON_PORT}/${process.env.MYSQL_ADDON_DB}`;
+const db = mysql.createPool(cleverCloudURL);
 
 // var db = mysql.createPool({
 //     host     : process.env.MYSQL_ADDON_HOST,
@@ -40,6 +40,6 @@ app.get('/getmovies', async (req, res) => {
     res.end();
 });
 
-app.listen(port, host, function () {
+app.listen(port, "0.0.0.0", function () {
     console.log(`listening on port: ${port}`)
 });
