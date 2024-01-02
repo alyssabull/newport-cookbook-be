4dd9e072a0e2844b3b33d8d5474bb0391bf5aa63
alyssabull <alyssabull@gmail.com>
2024-01-02 10:15:27 -0700

    Configure render

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = express();
 
 const port = process.env.DBPORT || 8080;
 const host = process.env.DBHOST || "0.0.0.0";
-const railwayURL = `mysql://ucxpdofnzajyujd8:4c7WqUFbmrYEnZQwSkW0@b32cy2ewfhbtxzwsgdpo-mysql.services.clever-cloud.com:3306/b32cy2ewfhbtxzwsgdpo`;
-const db = mysql.createPool(railwayURL);
+// const railwayURL = `mysql://ucxpdofnzajyujd8:4c7WqUFbmrYEnZQwSkW0@b32cy2ewfhbtxzwsgdpo-mysql.services.clever-cloud.com:3306/b32cy2ewfhbtxzwsgdpo`;
+// const db = mysql.createPool(railwayURL);
+
+var db = mysql.createConnection({
+    host     : process.env.MYSQL_ADDON_HOST,
+    database : process.env.MYSQL_ADDON_DB,
+    user     : process.env.MYSQL_ADDON_USER,
+    password : process.env.MYSQL_ADDON_PASSWORD
+});
 
 app.use(cors());
 app.use(express.json());
@@ -18,7 +25,7 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/getmovies', async (req, res) => {
-    const sqlGet = "SELECT * FROM `movie_reviews4`";
+    const sqlGet = "SELECT * FROM `b32cy2ewfhbtxzwsgdpo`";
     const result = await db.query(sqlGet);
     res.send(JSON.stringify(result));
     res.end();
