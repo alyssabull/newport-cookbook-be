fd28f8b9bbb124e881ad80fb4503ab5db0ef1769
alyssabull <alyssabull@gmail.com>
2024-01-01 18:00:11 -0700

    Add env variables

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
+const env = require("dotenv").config();
 const app = express();
 
+const port = process.env.MYSQLPORT || 3001;
+const railwayURL = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -13,13 +16,13 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("running on port 3001")
-});
-
 app.get('/getmovies', async (req, res) => {
     const sqlGet = "SELECT * FROM `movie_reviews4`";
     const result = await db.query(sqlGet);
     res.send(JSON.stringify(result));
     res.end();
+});
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`)
 });
