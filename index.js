4adbe54479958091eedbed7961b8c8e684b22ee2
alyssabull <alyssabull@gmail.com>
2024-01-03 13:57:38 -0700

    udfhso

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ app.get('/getrecipes', async (req, res) => {
 });
 
 app.post('/postnewrecipe', async (req, res) => {
+    console.log(req.body)
     const sqlInsert = "INSERT INTO `bzh9f8szz4sa4nts1m00`.`all_recipes` (dateAdded, title, description, details, instructions, picture, categories, isFavorite, notes, addedBy) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     db.query(sqlInsert, [req.body.dateAdded, req.body.title, req.body.description, req.body.details, req.body.instructions, req.body.picture, req.body.categories, req.body.isFavorite, req.body.notes, req.body.addedBy], async (err, result) => {
+        if (err) res.send(err);
         await res.send({data: "post successful"});
     });
     res.end(); 
@@ -35,5 +37,4 @@ app.post('/postnewrecipe', async (req, res) => {
 app.listen(8080, "0.0.0.0", function () {
     console.log("server up");
 });
-
 
