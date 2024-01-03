eba87eab367643cc6e9f780e31887fabf85bf7bf
alyssabull <alyssabull@gmail.com>
2024-01-02 21:10:40 -0700

    set up local development

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ const cleverCloudURL = `mysql://${process.env.MYSQL_ADDON_USER}:${process.env.MY
 const db = mysql.createPool(cleverCloudURL);
 
 app.use(cors({
-    origin: ["https://newport-cookbook.netlify.app", "https://newport-cookbook-be.cleverapps.io"]
+    origin: ["http://localhost:3000", "https://newport-cookbook.netlify.app", "https://newport-cookbook-be.cleverapps.io"]
 }));
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.send("testing");
+    res.send("server is working");
     res.end();
 });
 
-app.get('/getmovies', async (req, res) => {
+app.get('/getrecipes', async (req, res) => {
     const sqlGet = "SELECT * FROM bzh9f8szz4sa4nts1m00.all_recipes;";
     const result = await db.query(sqlGet);
     res.send(JSON.stringify(result));
     res.end();
+});
+
+app.post('/post', async (req, res) => {
+    const sqlInsert = "INSERT INTO `bzh9f8szz4sa4nts1m00`.`all_recipes` (`id`, `dateAdded`, `title`, `description`, `details`, `instructions`, `picture`, `categories`, `isFavorite`, `notes`, `addedBy`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    db.query(sqlInsert, [req.body.dateAdded, req.body.title, req.body.description, req.body.details, req.body.instructions, req.body.picture, req.body.categories, req.body.isFavorite, req.body.notes, req.body.addedBy], async (err, result) => {
+        if (err) throw err;
+        await res.send(result);
+    });
+    res.end(); 
 });
 
 app.listen(8080, "0.0.0.0", function () {
