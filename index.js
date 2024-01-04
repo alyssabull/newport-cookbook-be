1d444dc9d26568515c97ef86f03bf4760a0d8cdd
alyssabull <alyssabull@gmail.com>
2024-01-04 14:44:26 -0700

    revert

--- index.js
+++ index.js
@@ -26,42 +26,12 @@ app.get('/getrecipes', async (req, res) => {
 });
 
 app.post('/postnewrecipe', async (req, res) => {
-    const bufferData = Buffer.from(req.body.picture, 'base64');
-    fetch("https://api.tinify.com/shrink", bufferData, {
-      headers: {
-        'Content-Type': 'image/jpg',
-        'Authorization': 'Basic ' + Buffer.from(`api:${process.env.TINY_PNG_API_KEY}`).toString('base64'),
-      },
-    }).then((response) => {
-      const compressedData = response.data.output.buffer;
-      const finalCompression = zlib.deflateSync(compressedData);
-      res.send(finalCompression)
-    //   const sqlInsert = "INSERT INTO `bzh9f8szz4sa4nts1m00`.`all_recipes` (dateAdded, title, description, details, instructions, categories, isFavorite, notes, addedBy, picture) VALUES (?,?,?,?,?,?,?,?,?,?)";
-    //   db.query(sqlInsert, [req.body.dateAdded, req.body.title, req.body.description, req.body.details, req.body.instructions, req.body.categories, req.body.isFavorite, req.body.notes, req.body.addedBy, finalCompression], async (err, result) => {
-    //       if (err) res.send(err);
-    //       await res.send({data: "recipe post successful"});
-    //   });
+    const sqlInsert = "INSERT INTO `bzh9f8szz4sa4nts1m00`.`all_recipes` (dateAdded, title, description, details, instructions, categories, isFavorite, notes, addedBy, picture) VALUES (?,?,?,?,?,?,?,?,?,?)";
+    db.query(sqlInsert, [req.body.dateAdded, req.body.title, req.body.description, req.body.details, req.body.instructions, req.body.categories, req.body.isFavorite, req.body.notes, req.body.addedBy, finalCompression], async (err, result) => {
+        if (err) res.send(err);
+        await res.send({data: "recipe post successful"});
     });
     res.end(); 
-});
-
-app.post('/compressimage', async (req, res) => {
-    const imageBuffer = fs.readFileSync(req.body.picture);
-    axios.post("api.tinify.com", imageBuffer, {
-        headers: {
-          'Content-Type': 'image/png',
-          'Authorization': 'Basic ' + Buffer.from('api:' + `${TINY_PNG_API_KEY}`).toString('base64'),
-        },
-    }).then(response => {
-        // The compressed image data is available in response.data.output.buffer
-        const compressedData = response.data.output.buffer;
-    
-        // Save the compressed image to a new file or perform further actions
-        // fs.writeFileSync('path/to/your/compressed/image.png', compressedData);
-    
-        res.send(compressedData);
-    });
-    res.send();
 });
 
 app.delete('/deleterecipe/:id', async (req, res) => {
