0bf541bf14deae1f442465b8e98a14a008e57930
alyssabull <alyssabull@gmail.com>
2024-01-05 10:28:46 -0700

    skijf

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.get('/getrecipes', async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/images")
+        cb(null, path.join(__dirname, "public/images"))
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now().replace(/:/g, '-') + file.originalname)
+        cb(null, Date.now() + file.originalname)
     }
 });
 
