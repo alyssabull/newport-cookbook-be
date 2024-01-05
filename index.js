6f429bc0f37f12cc9835e88632ca29cc6c9e920b
alyssabull <alyssabull@gmail.com>
2024-01-05 10:09:39 -0700

    Change file path

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/getrecipes', async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/images")
+        cb(null, "./public/images")
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
