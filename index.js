a88ed5a9cffbdc08ccbf7c101aa836c250b8e78f
alyssabull <alyssabull@gmail.com>
2024-01-05 10:31:37 -0700

    tkae 1

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/getrecipes', async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "public/images"))
+        cb(null, path.join(__dirname, "/public/images"))
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + file.originalname)
